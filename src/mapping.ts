dbcaae2263aee4fe36c4de427578c14935d561a4
Leon Prouger <leonprou@gmail.com>
2020-05-10 21:03:05 +0300

    Add more data to Deposit and Withdraw entities

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,9 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { FundingCreated } from "../generated/FundingFactory/FundingFactory"
-import { Deposited } from "../generated/templates/Funding/Funding"
+import { Deposited, Withdrawn } from "../generated/templates/Funding/Funding"
+import { Funding as FundingDataSource } from "../generated/templates"
 
-import { Funding } from "../generated/schema"
+import { Funding, Deposit, Withdraw } from "../generated/schema"
 
 export function handleFundingCreated(event: FundingCreated): void {
   let entity = new Funding(event.params.funding.toHexString())
@@ -12,15 +13,40 @@ export function handleFundingCreated(event: FundingCreated): void {
   entity.owner = event.params.owner
   entity.operator = event.params.operator
   entity.interestToken = event.params.interestToken
-  entity.totalStaked = BigInt.fromI32(0)
 
   entity.createdAt = event.block.timestamp.toI32()
   entity.txHash = event.transaction.hash
+
+  FundingDataSource.create(event.params.funding)
 
   // Entities can be written to the store with `.save()`
   entity.save()
 }
 
-// export function handleDeposited(event: Deposited): void {
+export function handleDeposited(event: Deposited): void {
+  let id = event.transaction.hash.toHexString() + '_' + event.logIndex.toString() as string
 
-// }
+  let entity = new Deposit(id)
+  entity.sender = event.params.sender
+  entity.amount = event.params.amount
+
+  entity.funding = event.address
+  entity.createdAt = event.block.timestamp.toI32()
+  entity.txHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleWithdrawn(event: Withdrawn): void {
+  let id = event.transaction.hash.toHexString() + '_' + event.logIndex.toString() as string
+
+  let entity = new Withdraw(id)
+  entity.sender = event.params.sender
+  entity.amount = event.params.amount
+
+  entity.funding = event.address
+  entity.createdAt = event.block.timestamp.toI32()
+  entity.txHash = event.transaction.hash
+
+  entity.save()
+}
